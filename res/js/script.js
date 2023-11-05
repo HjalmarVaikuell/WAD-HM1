0955d1db3364db853fa01fefedacb7e247854faa
Hjalmar Vaikull <vaikuell.hjalmar@gmail.com>
2023-11-05 20:35:34 +0200

    Working button with white background

--- res/js/script.js
+++ res/js/script.js
@@ -1,12 +1,12 @@
 /* When the user clicks on the button, 
 toggle between hiding and showing the dropdown content */
-function myFunction() {
-  document.getElementById("dropdownMenu").classList.toggle("show");
+function dropdownFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
 }
 
 // Close the dropdown if the user clicks outside of it
 window.onclick = function(event) {
-  if (!event.target.matches('.menu-toggle')) {
+  if (!event.target.matches('.dropbtn')) {
     var dropdowns = document.getElementsByClassName("dropdown-content");
     var i;
     for (i = 0; i < dropdowns.length; i++) {
